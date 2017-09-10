f1dbe9db3d7816c4e98d79a86a6251e921b28bea
lokmeinmatz <matze.kind@web.de>
2017-09-10 12:52:15 +0200

    Created Menu and AI (not working yet)

---
+++ TickTackToe/AI.js
@@ -0,0 +1,190 @@
+const DIFFICULTY_EASY = 4123,
+      DIFFICULTY_HARD = 4131;
+
+function getAImove(board, difficulty){
+  if(difficulty == DIFFICULTY_EASY){
+    //return random free possible location
+    var moves = getPossibleMoves(board);
+    console.log(moves);
+    loadingTime = 0;
+    return moves[floor(random(moves.length))];
+  }
+  else if(difficulty == DIFFICULTY_HARD){
+    var choice = miniMax(board, 2, 0);
+    loadingTime = 0;
+    console.log("finished");
+    return choice;
+  }
+}
+
+function checkWinner(board){
+  //horizontal
+  for (var y = 0; y < 3; y++){
+    if(board[0][y] == board[1][y] && board[1][y] == board[2][y] && board[0][y] != 0){
+      return board[0][y];
+    }
+  }
+
+  //vertical
+  for (var x = 0; x < 3; x++){
+    if(board[x][0] == board[x][1] && board[x][1] == board[x][2] && board[x][0] != 0){
+      return board[x][0];
+    }
+  }
+
+  //diagonal 1
+  if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != 0){
+    return board[0][0];
+  }
+
+  //diagonal 1
+  if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != 0){
+    return board[0][2];
+  }
+
+  //check if field is full
+  for(var x = 0; x < 3; x++){
+    for(var y = 0; y < 3; y++){
+      if(board[x][y] == 0){
+        return 0;
+      }
+    }
+  }
+
+  return -1;
+
+}
+
+
+function miniMax(board){
+
+  function Node(data){
+    this.data = data;
+    this.parent = null;
+    this.children = [];
+  }
+
+  var rootdata = {
+    board: board,
+    score: 0,
+    depth: 0,
+    currentPlayer: 2,
+  };
+
+  var gamestate = checkWinner(board);
+  if(gamestate != 0){
+    if(gamestate == -1)rootdata.score =  0;
+    else if(gamestate == 2)rootdata.score = 10;
+    else if(gamestate == 1)rootdata.score = -10;
+  }
+
+  var root = new Node(rootdata);
+
+  var current = root;
+
+  while(current){
+    //generate possible moves
+    
+    var availableMoves = getPossibleMoves(current.data.board);
+
+
+  }
+
+  return;
+
+
+
+
+  scores = [];
+  moves = [];
+
+
+
+  for (var i = 0; i < availableMoves.length; i++) {
+    var move = availableMoves[i];
+    var mboard = copyBoard(oldBoard);
+    mboard[move.x][move.y] = 2;
+    scores.push(miniMax(mboard, (currentPlayer == 1)?2:1, depth));
+    moves.push(move);
+  }
+
+  if(currentPlayer == 2){
+    //max calculate
+    //get index of highest score
+    var max_index = indexOfMax(scores, false);
+    choice = moves[max_index];
+    return scores[max_index];
+  }
+  else{
+    //min calculate
+    //get index of lowest score
+    var low_index = indexOfMax(scores, true);
+    choice = moves[low_index];
+    return scores[low_index];
+  }
+
+
+}
+
+function indexOfMax(arr, min) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+      if(!min){
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+      }
+      else {
+        if (arr[i] < max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+      }
+    }
+
+    return maxIndex;
+}
+
+function copyBoard(board){
+  var newBoard = [];
+  for(var x = 0; x < 3; x++){
+    newBoard[x] = [];
+    for(var y = 0; y < 3; y++){
+      newBoard[x][y] = board[x][y];
+    }
+  }
+  return newBoard;
+}
+
+function getPossibleMoves(board){
+  moves = [];
+  for(var dx = 0; dx < 3; dx++){
+    for(var dy = 0; dy < 3; dy++){
+      if(board[dx][dy] == 0){
+        moves.push({x: dx, y: dy});
+      }
+    }
+  }
+  return moves;
+}
+
+var loadingTime = 0;
+
+function drawLoading(depth){
+  loadingTime++;
+  clear();
+
+  noStroke();
+  fill(50);
+  text("calculating move...", width/2, height/2);
+  text(depth+" | "+loadingTime, width/2, height/2 + 30);
+  ellipse(width/2, height/2 + 100, loadingTime%100);
+
+}
--- TickTackToe/sketch.js
+++ TickTackToe/sketch.js
@@ -1,11 +1,12 @@
 const ENDSCREEN = 31323,
       PLAYING = 31231,
+      MENU = 13123,
       CIRCLE = 1,
       CROSS = 2;
 
 const lineColor = "#0da192";
 
-var STATE = PLAYING;
+var STATE = MENU;
 
 var linelength = 0;
 
@@ -15,14 +16,26 @@ var data = [
   [0, 0, 0]
 ];
 
+var playAgainstAI = false;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight - 1);
 }
-
+var mouseInTouch = false;
 function draw() {
   clear();
+
+  if(mouseInTouch){
+    touches.splice(0, 1);
+    mouseInTouch = false;
+  }
+  if(mouseIsPressed){
+    mouseInTouch = true;
+    touches[0] = ({x:mouseX, y:mouseY});
+  }
+
+
   if(STATE == PLAYING){
     updateInputs();
     drawBorder();
@@ -35,7 +48,10 @@ function draw() {
     drawEndScreen();
   }
 
-  console.log(STATE);
+  else if(STATE == MENU){
+    updateInputsMenu();
+    drawMenu();
+  }
 
   if(winner != 0){
     STATE = ENDSCREEN;
@@ -53,32 +69,45 @@ function updateInputs(){
   var startY = (height - linelength)/2;
   var cellSize = linelength/3;
 
-  if (touches.length > 0 && clickcounter > 10) {
-    clickcounter = 0;
+  if(playAgainstAI && currentPlayer == CROSS){
 
-    var pressed_cell_x = floor((touches[0].x - startX) / cellSize);
-    var pressed_cell_y = floor((touches[0].y - startY) / cellSize);
-     text(pressed_cell_x + " " + pressed_cell_y, 20, 20);
-     if(pressed_cell_x < 0 || pressed_cell_y < 0 ||
-        pressed_cell_x >= 3 || pressed_cell_y >= 3)
-     {
-          //outside of map
-     }
-     else{
+    //do ai move
+    var move = getAImove(data, DIFFICULTY_HARD);
+    console.log(move);
+    if(data[move.x][move.y] == 0){
+      data[move.x][move.y] = currentPlayer;
 
-       if(data[pressed_cell_x][pressed_cell_y] == 0){
-         data[pressed_cell_x][pressed_cell_y] = currentPlayer;
+      currentPlayer = CIRCLE;
 
-         currentPlayer = (currentPlayer == CIRCLE)? CROSS : CIRCLE;
-
-         //check winner
-         winner = checkWinner();
-       }
-
-
+      //check winner
+      winner = checkWinner(data);
     }
   }
+  else{
+    if (touches.length > 0 && clickcounter > 10) {
+      clickcounter = 0;
 
+      var pressed_cell_x = floor((touches[0].x - startX) / cellSize);
+      var pressed_cell_y = floor((touches[0].y - startY) / cellSize);
+       text(pressed_cell_x + " " + pressed_cell_y, 20, 20);
+       if(pressed_cell_x < 0 || pressed_cell_y < 0 ||
+          pressed_cell_x >= 3 || pressed_cell_y >= 3)
+       {
+            //outside of map
+       }
+       else{
+
+         if(data[pressed_cell_x][pressed_cell_y] == 0){
+           data[pressed_cell_x][pressed_cell_y] = currentPlayer;
+
+           currentPlayer = (currentPlayer == CIRCLE)? CROSS : CIRCLE;
+
+           //check winner
+           winner = checkWinner(data);
+         }
+      }
+    }
+  }
 }
 
 
@@ -92,7 +121,7 @@ function updateInputsEndScreen(){
         for(var y = 0; y < 3; y++){
           data[x][y] = 0;
           winner = 0;
-          STATE = PLAYING;
+          STATE = MENU;
         }
       }
     }
@@ -107,8 +136,8 @@ function drawEndScreen(){
   textAlign(CENTER);
 
   if(winner > 0){
-    text("Herzlichen Glueckwunsch!", width/2, 70);
-    text("Spieler", width/2, 150);
+    text("Gratulation!", width/2, 70);
+    text("Player", width/2, 150);
 
     //draw winner
 
@@ -127,12 +156,12 @@ function drawEndScreen(){
     fill("white");
     noStroke();
     textAlign(CENTER);
-    text("hat gewonnen!", width/2, 200 + cellSize * 1.2);
+    text("won the game!", width/2, 200 + cellSize * 1.2);
   }
 
   else{
     textSize(30);
-    text("Gleichstand!",width/2, height/2);
+    text("Draw!",width/2, height/2);
     textSize(15);
   }
 
@@ -141,52 +170,16 @@ function drawEndScreen(){
   rect(0, height - 70, width, height);
 
   fill("#14bdac");
-  text("Neues Spiel", width/2, height - 30);
+  text("Back to menu", width/2, height - 30);
 
 }
 
-function checkWinner(){
-  //horizontal
-  for (var y = 0; y < 3; y++){
-    if(data[0][y] == data[1][y] && data[1][y] == data[2][y] && data[0][y] != 0){
-      return data[0][y];
-    }
-  }
 
-  //vertical
-  for (var x = 0; x < 3; x++){
-    if(data[x][0] == data[x][1] && data[x][1] == data[x][2] && data[x][0] != 0){
-      return data[x][0];
-    }
-  }
 
-  //diagonal 1
-  if(data[0][0] == data[1][1] && data[1][1] == data[2][2] && data[0][0] != 0){
-    return data[0][0];
-  }
-
-  //diagonal 1
-  if(data[0][2] == data[1][1] && data[1][1] == data[2][0] && data[0][2] != 0){
-    return data[0][2];
-  }
-
-  //check if field is full
-  for(var x = 0; x < 3; x++){
-    for(var y = 0; y < 3; y++){
-      if(data[x][y] == 0){
-        return 0;
-      }
-    }
-  }
-
-  return -1;
-
-}
-
-function drawCell(x, y, type){
+function drawCell(x, y, type, cellSize){
   var startX = (width -  linelength)/2 + x * (linelength/3);
   var startY = (height - linelength)/2 + y * (linelength/3);
-  var cellSize = linelength/3;
+  noFill();
 
   if (type == CIRCLE){
 
@@ -207,7 +200,7 @@ function drawMap(){
   noFill();
   for (var x = 0; x < 3; x++){
     for (var y = 0; y < 3; y++){
-      drawCell(x, y, data[x][y]);
+      drawCell(x, y, data[x][y], linelength/3);
     }
   }
 }
@@ -237,4 +230,97 @@ function drawBorder(){
 
   //right vertical
   line(startX + 2*linelength/3, startY, startX + 2*linelength/3, startY + linelength);
+}
+
+var menuData = {
+  targetOffsetX: 0,
+  currentOffsetX: 0,
+  mouseHoverAI: false,
+  clickTimer: 10,
+  mouseHoverPlay: false,
+}
+
+function updateInputsMenu() {
+  menuData.clickTimer--;
+  if(touches.length > 0 && menuData.clickTimer < 0){
+    menuData.clickTimer = 10;
+    if(touches[0].y >= 200 && touches[0].y <= 300){
+      //inside player area
+      if(touches[0].x < width/2)currentPlayer = CIRCLE;
+      else {
+        currentPlayer = CROSS;
+      }
+
+    }
+    else if(touches[0].y >= 450 && touches[0].y <= 490 && touches[0].x >= width/2 - 20 && touches[0].x <= width/2 + 20)playAgainstAI = !playAgainstAI;
+    else if(touches[0].y >= height - 90 && touches[0].y <= height - 30 && touches[0].x >= width/2 - 70 && touches[0].x <= width/2 + 70)STATE = PLAYING
+  }
+  if(mouseY >= 450 && mouseY <= 490 && mouseX >= width/2 - 20 && mouseX <= width/2 + 20)menuData.mouseHoverAI = true;
+  else {
+    menuData.mouseHoverAI = false;
+  }
+
+  if(mouseY >= height - 90 && mouseY <= height - 30 && mouseX >= width/2 - 70 && mouseX <= width/2 + 70)menuData.mouseHoverPlay = true;
+  else {
+    menuData.mouseHoverPlay = false;
+  }
+}
+
+function drawMenu() {
+textAlign(CENTER);
+  textSize(30);
+  var s = "Do you want Cross or Circle to start?"
+  fill(50);
+  text(s, width/2, 100);
+
+  //draw starting or ai
+  noStroke();
+  fill(200);
+  rect(0, 200, width, 100);
+
+  menuData.targetOffsetX = (currentPlayer == CIRCLE)? 0 : width/2;
+  menuData.currentOffsetX = lerp(menuData.currentOffsetX, menuData.targetOffsetX, 0.1);
+  fill(250);
+  rect(menuData.currentOffsetX, 200, width/2, 100);
+  noFill();
+  stroke(84, 84, 84);
+  ellipse(width/4 + 50, 250, 70);
+
+
+  var startX = width / 4 * 3,
+      startY = 200,
+      cellSize = 100;
+  line(startX + cellSize*0.2, startY + cellSize* 0.2, startX + cellSize*0.8, startY + cellSize*0.8);
+  line(startX + cellSize*0.8, startY + cellSize* 0.2, startX + cellSize*0.2, startY + cellSize*0.8);
+
+
+  //ai checkbox
+  noStroke();
+  fill(50);
+  text("Play against AI (AI will be Cross)", width/2, 400);
+
+  stroke(50);
+  strokeWeight(1);
+  if(menuData.mouseHoverAI)strokeWeight(4);
+  noFill();
+  rect(width/2 - 20, 450, 40, 40);
+
+  strokeWeight(4);
+  if(playAgainstAI){
+    //draw check
+    line(width/2 - 15, 470, width/2, 485);
+    line(width/2, 485, width/2 + 25, 455);
+  }
+
+  strokeWeight(1);
+  noStroke();
+
+
+  //play button
+  noStroke();
+  if(menuData.mouseHoverPlay)stroke(50);
+  fill(230);
+  rect(width/2 - 70, height - 90, 140, 60);
+  fill(50);
+  text("PLAY", width/2, height - 50);
 }
