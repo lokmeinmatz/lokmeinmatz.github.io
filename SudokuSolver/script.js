f7e3c5d98c90b7f1480dc0b762e282c7dc299e5e
lokmeinmatz <matze.kind@web.de>
2017-10-17 20:52:27 +0200

    Updated SudokuSolver

--- SudokuSolver/script.js
+++ SudokuSolver/script.js
@@ -5,8 +5,85 @@ let data = [];
 
 let validInputs = false;
 
+function solved(data) {
+  
+}
+
 function solve(){
 
+}
+
+let STATES = {
+  VALID: 1,
+  INVALID: -1,
+  EMPTY: 0,
+};
+
+class Cell {
+
+  constructor(x, y, final){
+    this.x = x;
+    this.final = final;
+    this.y = y;
+    this.value = 0;
+    this.html = null;
+    this.possible = [];
+  }
+
+
+  setState(state){
+    switch (state) {
+      case STATES.EMPTY:
+        this.html.removeClass("valid");
+        this.html.removeClass("invalid");
+        if(!this.html.hasClass("empty"))this.html.addClass("empty");
+        break;
+
+      case STATES.VALID:
+        this.html.removeClass("invalid");
+        this.html.removeClass("empty");
+        if(!this.html.hasClass("valid"))this.html.addClass("valid");
+        break;
+
+      case STATES.INVALID:
+        this.html.removeClass("valid");
+        this.html.removeClass("empty");
+        if(!this.html.hasClass("invalid"))this.html.addClass("invalid");
+        break;
+
+      default:
+
+    }
+  }
+}
+
+function getPeers(x, y) {
+  let peers = [];
+
+  //add all off row
+  for(tx = 0; tx < 9; tx++){
+    if(tx != x){
+      peers.push(data[tx][y]);
+    }
+  }
+
+  //add all off column
+  for(ty = 0; ty < 9; ty++){
+    if(ty != y){
+      peers.push(data[x][ty]);
+    }
+  }
+
+  //add all from box
+  let startx = Math.floor(x / 3) * 3;
+  let starty = Math.floor(y / 3) * 3;
+  for(let dx = startx; dx < startx + 3; dx++){
+    for(let dy = starty; dy < starty + 3; dy++){
+      if(!(dx == x && dy == y) && peers.indexOf(data[dx][dy]) == -1)peers.push(data[dx][dy]);
+    }
+  }
+
+  return peers;
 }
 
 $(function(){
@@ -38,29 +115,27 @@ function validate(){
   for(let x = 0; x < 9; x++)data.push([]);
   validInputs = true;
   for(let i = 0; i < table.length; i++){
-    data[i % 9][(i - (i % 9)) / 9] = table[i].val();
+    //generate coord
+    let c = new Cell(i % 9, (i - (i % 9)) / 9, table[i].val()?true:false);
+    c.value = table[i].val();
+    c.html = table[i]
+    data[c.x][c.y] = c;
     if(table[i].val()){
       //not null
       if(table[i].val() > 0 && table[i].val() <= 9){
         //valid
-        table[i].removeClass("invalid");
-        table[i].removeClass("empty");
-        if(!table[i].hasClass("valid"))table[i].addClass("valid");
+        c.setState(STATES.VALID)
       }
       else{
         //invald input
         //no input
-        table[i].removeClass("valid");
-        table[i].removeClass("empty");
-        if(!table[i].hasClass("invalid"))table[i].addClass("invalid");
+        c.setState(STATES.INVALID)
         validInputs = false;
       }
     }
     else{
       //no input
-      table[i].removeClass("valid");
-      table[i].removeClass("invalid");
-      if(!table[i].hasClass("empty"))table[i].addClass("empty");
+      c.setState(STATES.EMPTY)
     }
   }
 
@@ -68,30 +143,27 @@ function validate(){
   for(let x = 0; x < 9; x++){
     for(let y = 0; y < 9; y++){
       let current = data[x][y];
-      let i = (y * 9) + x;
-      if(!current)continue;
-      //test row
-      for(tx = 0; tx < 9; tx++){
-        if(tx != x && data[tx][y] == current){
-          //exists
-          table[i].removeClass("valid");
-          table[i].removeClass("empty");
-          if(!table[i].hasClass("invalid"))table[i].addClass("invalid");
-          validInputs = false;
-        }
-      }
 
-      //test column
-      for(ty = 0; ty < 9; ty++){
-        if(ty != y && data[ty][y] == current){
-          //exists
-          table[i].removeClass("valid");
-          table[i].removeClass("empty");
-          if(!table[i].hasClass("invalid"))table[i].addClass("invalid");
+      if(!current || !current.value)continue;
+
+      let peers = getPeers(x, y);
+      for(let i = 0; i < peers.length; i++){
+        let f = peers[i];
+        if(f.value == current.value){
+          current.setState(STATES.INVALID);
           validInputs = false;
+          break;
         }
       }
 
     }
   }
+
+  //test
+  let peers = getPeers(1, 1);
+  console.log(peers);
+
+  peers.forEach(f => {
+
+  });
 }
