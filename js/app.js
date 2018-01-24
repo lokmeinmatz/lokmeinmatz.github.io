7d5b3d537648c66f056f71cc7b8ecf15589160c5
lokmeinmatz <matze.kind@web.de>
2018-01-24 12:06:01 +0100

    Added Photography

--- js/app.js
+++ js/app.js
@@ -7,9 +7,17 @@ $(document).ready(function() {
     addTile("MineSweeper", "#games-list")
     addTile("TickTackToe", "#games-list")
 
-    //add UI experiments
-    addTile("Who is the wolf?!", "#uiexp-list", "Wer_istder_Wolf")
-    addTile("TODO second ui page", "#uiexp-list")
+    //add Programming experiments
+    addTile("Who is the wolf?!", "#prexp-list", "Wer_istder_Wolf")
+    addTile("Shadow2D", "#prexp-list")
+    addTile("RacingGame", "#prexp-list")
+    addTile("Snake", "#prexp-list")
+
+    //add Photography experiments
+    addTile("Architecture", "#photo-list")
+    addTile("Sports", "#photo-list")
+    addTile("Nature", "#photo-list")
+    addTile("People", "#photo-list")
 
 })
 
@@ -18,7 +26,20 @@ const ProjectDescriptions = new Map();
 ProjectDescriptions.set("MineSweeper", "Just a JS-Implementation of MineSweeper, not all features there. Works also on mobile.")
 ProjectDescriptions.set("TickTackToe", "I think you know Tick Tack Toe, but this one has an AI included! Works also on mobile.")
 
+ProjectDescriptions.set("Shadow2D", "Method to render 2d Shadows (for Poligons)... Sourcecode also contains 'random' raycast method.")
 ProjectDescriptions.set("Who is the wolf?!", "Design Experiment for an online-game.")
+
+ProjectDescriptions.set("RacingGame", "2D Retro Game ported from C++ (by javidx9).")
+ProjectDescriptions.set("Snake", "2D Pixel Snake, currently working on AI with A* Pathfinding.")
+
+//photography
+ProjectDescriptions.set("Architecture", "Timeless images which capture the beauty of human created buildings.")
+
+ProjectDescriptions.set("Sports", "People in action, pushing the limits.")
+
+ProjectDescriptions.set("Nature", "Our planet provides great motives by itself. My favourites: sunsets and the ocean.")
+
+ProjectDescriptions.set("People", "Streetphotography or Portraits : both interesting areas.")
 
 function getDesc(name) {
     if(ProjectDescriptions.has(name)) return ProjectDescriptions.get(name)
