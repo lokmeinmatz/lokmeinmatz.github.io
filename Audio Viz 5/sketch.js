21d514df54f18a6aa9f43a554d2259ff82833fc9
lokmeinmatz <matze.kind@web.de>
2018-03-02 14:34:52 +0100

    changed v5

--- Audio Viz 5/sketch.js
+++ Audio Viz 5/sketch.js
@@ -20,12 +20,36 @@ function updateCanvasScale() {
 let canvas
 
 
+let root
+
+class Branch {
+  constructor(previous, angle, depth, angleVar) {
+    this.base = previous.top
+    this.length = previous.length * 0.75
+    this.top = this.base.copy()
+    this.top.add(p5.Vector.fromAngle(angle).mult(this.length))
+    this.depth = depth
+    if(depth < 8) {
+      this.left = new Branch(this, angle + angleVar*1, depth+1, angleVar) 
+      this.right = new Branch(this, angle - angleVar*1, depth+1, angleVar) 
+    }
+  }
+
+  draw() {
+    stroke(this.depth*5, 255 - this.depth*5,250)
+    line(this.base.x, this.base.y, this.top.x, this.top.y)
+    if(this.left) this.left.draw()
+    if(this.right) this.right.draw()
+  }
+}
+let time = 0.0
 function setup() {
   canvas = createCanvas(1800, 1000);
   updateCanvasScale()
   angleMode(DEGREES);
   console.log("Starting program...");
   
+  root = new Branch({top:createVector(width/2, height-50), length:400}, PI*1.5, 0, time)
   
 }
 
@@ -34,7 +58,10 @@ function draw() {
   background(100);
   colorMode(HSB);
   stroke(255)
-
+  strokeWeight(3)
+  time += 0.01
+  root = new Branch({top:createVector(width/2, height-50), length:400}, PI*1.5, 0, time)
+  root.draw()
 
  
 }
