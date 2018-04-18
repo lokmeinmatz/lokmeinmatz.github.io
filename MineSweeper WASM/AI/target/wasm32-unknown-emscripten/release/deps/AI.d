76cae78ce7fdcb9b658a6e67c93584866c4c23c1
lokmeinmatz <matze.kind@web.de>
2018-04-19 00:03:30 +0200

    rm WASM minesweeper ai

--- MineSweeper WASM/AI/target/wasm32-unknown-emscripten/release/deps/AI.d
+++
@@ -1,5 +0,0 @@
-D:\Programmieren\GitHub\lokmeinmatz.github.io\MineSweeper WASM\AI\target\wasm32-unknown-emscripten\release\deps\AI.js: src\main.rs
-
-D:\Programmieren\GitHub\lokmeinmatz.github.io\MineSweeper WASM\AI\target\wasm32-unknown-emscripten\release\deps\AI.d: src\main.rs
-
-src\main.rs:
--- MineSweeper WASM/mineSweeperAI/src/lib.rs
+++
@@ -1,6 +0,0 @@
-
-
-#[no_mangle]
-pub extern fn add(a: u32) -> u32 {
-  a + 1
-}
