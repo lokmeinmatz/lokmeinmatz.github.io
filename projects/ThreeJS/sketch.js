8be7c72fbede269219f97333b277ad1214834a0b
lokmeinmatz <matze.kind@web.de>
2018-04-25 12:23:40 +0200

    working

--- projects/ThreeJS/sketch.js
+++ projects/ThreeJS/sketch.js
@@ -21,25 +21,26 @@ function map(val, start1, stop1, start2, stop2) {
 }
 
 
+mountains = []
 
+window.onresize = windowResized
 
 function windowResized() {
-  let canvasstyle = document.getElementsByTagName("canvas")[0].style;
-  let windowRatio = window.innerwidth / window.innerheight
-  let canvasRatio = width / height
-  if (windowRatio < canvasRatio) {
-    canvasstyle.width = "100vw";
-    canvasstyle.height = "auto";
-  }
-  else {
-    canvasstyle.width = "auto";
-    canvasstyle.height = "100vh";
-  }
+  //let canvasstyle = document.getElementsByTagName("canvas")[0].style;
+  
+  width = window.innerWidth
+  height = window.innerHeight
+
+  cam.aspect = width / height
+  cam.updateProjectionMatrix()
+  
+  renderer.setSize(width, height)
+  console.log(renderer.getSize())
 }
 
-const width = 400 * 2
-const height = 300 * 2
-
+let width = 400 * 2
+let height = 300 * 2
+let composer
 
 var cam, renderer
 
@@ -49,9 +50,23 @@ function generateMountains(scale) {
   let THREEgeometry = new THREE.Geometry()
   THREEgeometry.faceVertexUvs = [[]]
 
+  var attributes = {
+    displacement: {
+      type: 'f', // a float
+      value: [] // an empty array
+    }
+  };
+  let lastheight = 1.0
+
   for (let i = 0; i < 10; i++) {
-    THREEgeometry.vertices.push(new THREE.Vector3(i * scale, Math.random() * scale + scale, 0))
-    THREEgeometry.vertices.push(new THREE.Vector3(i * scale, 0, 0))
+    let nheight
+
+    do {
+      nheight = Math.random()
+    } while (Math.abs(lastheight - nheight) < 0.3);
+    lastheight = nheight
+    THREEgeometry.vertices.push(new THREE.Vector3(i * scale, nheight * scale + scale, 0))
+    THREEgeometry.vertices.push(new THREE.Vector3(i * scale, -scale, 0))
 
     if (i > 0) {
       let fi = i * 2 - 2
@@ -63,7 +78,7 @@ function generateMountains(scale) {
       THREEgeometry.faces.push(new THREE.Face3(fi + 1, fi + 3, fi + 2))
 
       //tri 1
-      const uvScale = 2
+      const uvScale = 1
 
 
       //get vectors
@@ -77,11 +92,14 @@ function generateMountains(scale) {
       let lastX = map(lastTop.x, 0, width, 0, 1) * uvScale
       let lastY = map(lastTop.y, 0, width, 0.0, 1.0) * uvScale
 
-
+      if(true) {
+        thisY = 1.0
+        lastY = 1.0
+      }
 
       //test
-      //thisY = 0
-      //lastY = 0
+      thisY = 1
+      lastY = 1
 
       THREEgeometry.faceVertexUvs[0].push([
         new THREE.Vector2(lastX, lastY),
@@ -99,20 +117,19 @@ function generateMountains(scale) {
     }
   }
 
+
+
+
   
-  let THREEobj = new THREE.Mesh(THREEgeometry, new THREE.MeshBasicMaterial({}))
-  THREEobj.position.z = -100
+  let THREEobj = new THREE.Mesh(THREEgeometry, shader)
+  THREEobj.position.z = -500
   THREEobj.position.x = -4.5 * scale
   THREEobj.position.y = - scale
   //this.THREEobj = new THREE.Mesh(THREEgeometry, new THREE.PointsMaterial())
   scene.add(THREEobj)
   
   
-  THREEobj.material.transparent = true
-  THREEobj.material.map = gradTexture
-  THREEobj.material.depthWrite = false
-  THREEobj.material.flatShading = true
-  THREEobj.material.premultipliedAlpha = true
+
   console.log(THREEobj.material)
   //THREEobj.material.alphaTest = 0.1
   //THREEobj.material.map = new THREE.TextureLoader().load("imgs/dirt.jpg");
@@ -121,19 +138,59 @@ function generateMountains(scale) {
 }
 
 window.onload = init
+let shader
 
+window.onkeypress = function(e) {
+  if(e.key == 'f'){
+    if( THREEx.FullScreen.activated() ){
+      THREEx.FullScreen.cancel();
+    }else{
+      THREEx.FullScreen.request();
+    }
+    
+  }
+}
 
-let gradTexture = new THREE.TextureLoader().load( "grad.png" )
 function init() {
 
   //TODO replace with background
   scene.background = new THREE.Color(0, 0, 0)
 
+  width = window.innerWidth
+  height = window.innerHeight
+
 
   cam = new THREE.PerspectiveCamera(70, width / height, 0.1, 1000)
-  renderer = new THREE.WebGLRenderer()
+  cam.position.y = 100;
+  renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setSize(width, height)
+  renderer.autoClear = false
+
+
+  composer = new THREE.EffectComposer(renderer)
+
+  let renderPass = new THREE.RenderPass(scene, cam);
+  composer.addPass(renderPass);
+
+  let bloomPass = new THREE.BloomPass();
+  composer.addPass(bloomPass);
+  
+  let effectCopy = new THREE.ShaderPass(THREE.CopyShader)
+  effectCopy.renderToScreen = true  
+  composer.addPass(effectCopy)
+
   document.body.appendChild(renderer.domElement)
+
+  //load shader
+  let vert = document.getElementById('vertex').innerText
+  let frag = document.getElementById('fragment').innerText
+  //console.log(vert, frag)
+
+  shader = new THREE.ShaderMaterial({
+    vertexShader: vert,
+    fragmentShader: frag,
+    blending: THREE.NormalBlending
+  })
 
   windowResized()
 
@@ -143,8 +200,22 @@ function init() {
 
 function update() {
 
+  if(mountains.length < 1 || mountains[mountains.length - 1].position.z > -400) {
+    mountains.push(generateMountains(90))
+  }
+  for(let i = 0; i < mountains.length; i++) {
+    mountains[i].position.z += 0.3
+    if(mountains[i].position.z > -0.1) {
+      mountains.splice(i, 1)
+    }
+  }
 
-  renderer.render(scene, cam)
+  
+
+
+  composer.render();
 
   requestAnimationFrame(update)
 }
+
+
---
+++ projects/ThreeJS/threeHelper.js
@@ -0,0 +1,678 @@
+
+//from github threejs examples
+
+
+/**
+ * @description EffectComposer
+ * @author alteredq / http://alteredqualia.com/
+ */
+
+THREE.EffectComposer = function ( renderer, renderTarget ) {
+
+	this.renderer = renderer;
+
+	if ( renderTarget === undefined ) {
+
+		var parameters = {
+			minFilter: THREE.LinearFilter,
+			magFilter: THREE.LinearFilter,
+			format: THREE.RGBAFormat,
+			stencilBuffer: false
+		};
+
+		var size = renderer.getDrawingBufferSize();
+		renderTarget = new THREE.WebGLRenderTarget( size.width, size.height, parameters );
+		renderTarget.texture.name = 'EffectComposer.rt1';
+
+	}
+
+	this.renderTarget1 = renderTarget;
+	this.renderTarget2 = renderTarget.clone();
+	this.renderTarget2.texture.name = 'EffectComposer.rt2';
+
+	this.writeBuffer = this.renderTarget1;
+	this.readBuffer = this.renderTarget2;
+
+	this.passes = [];
+
+	// dependencies
+
+	if ( THREE.CopyShader === undefined ) {
+
+		console.error( 'THREE.EffectComposer relies on THREE.CopyShader' );
+
+	}
+
+	if ( THREE.ShaderPass === undefined ) {
+
+		console.error( 'THREE.EffectComposer relies on THREE.ShaderPass' );
+
+	}
+
+	this.copyPass = new THREE.ShaderPass( THREE.CopyShader );
+
+};
+
+Object.assign( THREE.EffectComposer.prototype, {
+
+	swapBuffers: function () {
+
+		var tmp = this.readBuffer;
+		this.readBuffer = this.writeBuffer;
+		this.writeBuffer = tmp;
+
+	},
+
+	addPass: function ( pass ) {
+
+		this.passes.push( pass );
+
+		var size = this.renderer.getDrawingBufferSize();
+		pass.setSize( size.width, size.height );
+
+	},
+
+	insertPass: function ( pass, index ) {
+
+		this.passes.splice( index, 0, pass );
+
+	},
+
+	render: function ( delta ) {
+
+		var maskActive = false;
+
+		var pass, i, il = this.passes.length;
+
+		for ( i = 0; i < il; i ++ ) {
+
+			pass = this.passes[ i ];
+
+			if ( pass.enabled === false ) continue;
+
+			pass.render( this.renderer, this.writeBuffer, this.readBuffer, delta, maskActive );
+
+			if ( pass.needsSwap ) {
+
+				if ( maskActive ) {
+
+					var context = this.renderer.context;
+
+					context.stencilFunc( context.NOTEQUAL, 1, 0xffffffff );
+
+					this.copyPass.render( this.renderer, this.writeBuffer, this.readBuffer, delta );
+
+					context.stencilFunc( context.EQUAL, 1, 0xffffffff );
+
+				}
+
+				this.swapBuffers();
+
+			}
+
+			if ( THREE.MaskPass !== undefined ) {
+
+				if ( pass instanceof THREE.MaskPass ) {
+
+					maskActive = true;
+
+				} else if ( pass instanceof THREE.ClearMaskPass ) {
+
+					maskActive = false;
+
+				}
+
+			}
+
+		}
+
+	},
+
+	reset: function ( renderTarget ) {
+
+		if ( renderTarget === undefined ) {
+
+			var size = this.renderer.getDrawingBufferSize();
+
+			renderTarget = this.renderTarget1.clone();
+			renderTarget.setSize( size.width, size.height );
+
+		}
+
+		this.renderTarget1.dispose();
+		this.renderTarget2.dispose();
+		this.renderTarget1 = renderTarget;
+		this.renderTarget2 = renderTarget.clone();
+
+		this.writeBuffer = this.renderTarget1;
+		this.readBuffer = this.renderTarget2;
+
+	},
+
+	setSize: function ( width, height ) {
+
+		this.renderTarget1.setSize( width, height );
+		this.renderTarget2.setSize( width, height );
+
+		for ( var i = 0; i < this.passes.length; i ++ ) {
+
+			this.passes[ i ].setSize( width, height );
+
+		}
+
+	}
+
+} );
+
+
+THREE.Pass = function () {
+
+	// if set to true, the pass is processed by the composer
+	this.enabled = true;
+
+	// if set to true, the pass indicates to swap read and write buffer after rendering
+	this.needsSwap = true;
+
+	// if set to true, the pass clears its buffer before rendering
+	this.clear = false;
+
+	// if set to true, the result of the pass is rendered to screen
+	this.renderToScreen = false;
+
+};
+
+Object.assign( THREE.Pass.prototype, {
+
+	setSize: function ( width, height ) {},
+
+	render: function ( renderer, writeBuffer, readBuffer, delta, maskActive ) {
+
+		console.error( 'THREE.Pass: .render() must be implemented in derived pass.' );
+
+	}
+
+} );
+
+/**
+ * @description CopyShader
+ * @author alteredq / http://alteredqualia.com/
+ *
+ * Full-screen textured quad shader
+ */
+
+THREE.CopyShader = {
+
+	uniforms: {
+
+		"tDiffuse": { value: null },
+		"opacity":  { value: 1.0 }
+
+	},
+
+	vertexShader: [
+
+		"varying vec2 vUv;",
+
+		"void main() {",
+
+			"vUv = uv;",
+			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+
+		"}"
+
+	].join( "\n" ),
+
+	fragmentShader: [
+
+		"uniform float opacity;",
+
+		"uniform sampler2D tDiffuse;",
+
+		"varying vec2 vUv;",
+
+		"void main() {",
+
+			"vec4 texel = texture2D( tDiffuse, vUv );",
+			"gl_FragColor = opacity * texel;",
+
+		"}"
+
+	].join( "\n" )
+
+};
+
+/**
+ * @description RenderPass
+ * @author alteredq / http://alteredqualia.com/
+ */
+
+THREE.RenderPass = function ( scene, camera, overrideMaterial, clearColor, clearAlpha ) {
+
+	THREE.Pass.call( this );
+
+	this.scene = scene;
+	this.camera = camera;
+
+	this.overrideMaterial = overrideMaterial;
+
+	this.clearColor = clearColor;
+	this.clearAlpha = ( clearAlpha !== undefined ) ? clearAlpha : 0;
+
+	this.clear = true;
+	this.clearDepth = false;
+	this.needsSwap = false;
+
+};
+
+THREE.RenderPass.prototype = Object.assign( Object.create( THREE.Pass.prototype ), {
+
+	constructor: THREE.RenderPass,
+
+	render: function ( renderer, writeBuffer, readBuffer, delta, maskActive ) {
+
+		var oldAutoClear = renderer.autoClear;
+		renderer.autoClear = false;
+
+		this.scene.overrideMaterial = this.overrideMaterial;
+
+		var oldClearColor, oldClearAlpha;
+
+		if ( this.clearColor ) {
+
+			oldClearColor = renderer.getClearColor().getHex();
+			oldClearAlpha = renderer.getClearAlpha();
+
+			renderer.setClearColor( this.clearColor, this.clearAlpha );
+
+		}
+
+		if ( this.clearDepth ) {
+
+			renderer.clearDepth();
+
+		}
+
+		renderer.render( this.scene, this.camera, this.renderToScreen ? null : readBuffer, this.clear );
+
+		if ( this.clearColor ) {
+
+			renderer.setClearColor( oldClearColor, oldClearAlpha );
+
+		}
+
+		this.scene.overrideMaterial = null;
+		renderer.autoClear = oldAutoClear;
+	}
+
+} );
+
+/**
+ * @description Shaderpass
+ * @author alteredq / http://alteredqualia.com/
+ */
+
+THREE.ShaderPass = function ( shader, textureID ) {
+
+	THREE.Pass.call( this );
+
+	this.textureID = ( textureID !== undefined ) ? textureID : "tDiffuse";
+
+	if ( shader instanceof THREE.ShaderMaterial ) {
+
+		this.uniforms = shader.uniforms;
+
+		this.material = shader;
+
+	} else if ( shader ) {
+
+		this.uniforms = THREE.UniformsUtils.clone( shader.uniforms );
+
+		this.material = new THREE.ShaderMaterial( {
+
+			defines: Object.assign( {}, shader.defines ),
+			uniforms: this.uniforms,
+			vertexShader: shader.vertexShader,
+			fragmentShader: shader.fragmentShader
+
+		} );
+
+	}
+
+	this.camera = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
+	this.scene = new THREE.Scene();
+
+	this.quad = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2, 2 ), null );
+	this.quad.frustumCulled = false; // Avoid getting clipped
+	this.scene.add( this.quad );
+
+};
+
+THREE.ShaderPass.prototype = Object.assign( Object.create( THREE.Pass.prototype ), {
+
+	constructor: THREE.ShaderPass,
+
+	render: function( renderer, writeBuffer, readBuffer, delta, maskActive ) {
+
+		if ( this.uniforms[ this.textureID ] ) {
+
+			this.uniforms[ this.textureID ].value = readBuffer.texture;
+
+		}
+
+		this.quad.material = this.material;
+
+		if ( this.renderToScreen ) {
+
+			renderer.render( this.scene, this.camera );
+
+		} else {
+
+			renderer.render( this.scene, this.camera, writeBuffer, this.clear );
+
+		}
+
+	}
+
+} );
+
+/**
+ * @author alteredq / http://alteredqualia.com/
+ */
+
+THREE.BloomPass = function ( strength, kernelSize, sigma, resolution ) {
+
+	THREE.Pass.call( this );
+
+	strength = ( strength !== undefined ) ? strength : 1;
+	kernelSize = ( kernelSize !== undefined ) ? kernelSize : 25;
+	sigma = ( sigma !== undefined ) ? sigma : 4.0;
+	resolution = ( resolution !== undefined ) ? resolution : 256;
+
+	// render targets
+
+	var pars = { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat };
+
+	this.renderTargetX = new THREE.WebGLRenderTarget( resolution, resolution, pars );
+	this.renderTargetX.texture.name = "BloomPass.x";
+	this.renderTargetY = new THREE.WebGLRenderTarget( resolution, resolution, pars );
+	this.renderTargetY.texture.name = "BloomPass.y";
+
+	// copy material
+
+	if ( THREE.CopyShader === undefined )
+		console.error( "THREE.BloomPass relies on THREE.CopyShader" );
+
+	var copyShader = THREE.CopyShader;
+
+	this.copyUniforms = THREE.UniformsUtils.clone( copyShader.uniforms );
+
+	this.copyUniforms[ "opacity" ].value = strength;
+
+	this.materialCopy = new THREE.ShaderMaterial( {
+
+		uniforms: this.copyUniforms,
+		vertexShader: copyShader.vertexShader,
+		fragmentShader: copyShader.fragmentShader,
+		blending: THREE.AdditiveBlending,
+		transparent: true
+
+	} );
+
+	// convolution material
+
+	if ( THREE.ConvolutionShader === undefined )
+		console.error( "THREE.BloomPass relies on THREE.ConvolutionShader" );
+
+	var convolutionShader = THREE.ConvolutionShader;
+
+	this.convolutionUniforms = THREE.UniformsUtils.clone( convolutionShader.uniforms );
+
+	this.convolutionUniforms[ "uImageIncrement" ].value = THREE.BloomPass.blurX;
+	this.convolutionUniforms[ "cKernel" ].value = THREE.ConvolutionShader.buildKernel( sigma );
+
+	this.materialConvolution = new THREE.ShaderMaterial( {
+
+		uniforms: this.convolutionUniforms,
+		vertexShader:  convolutionShader.vertexShader,
+		fragmentShader: convolutionShader.fragmentShader,
+		defines: {
+			"KERNEL_SIZE_FLOAT": kernelSize.toFixed( 1 ),
+			"KERNEL_SIZE_INT": kernelSize.toFixed( 0 )
+		}
+
+	} );
+
+	this.needsSwap = false;
+
+	this.camera = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
+	this.scene  = new THREE.Scene();
+
+	this.quad = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2, 2 ), null );
+	this.quad.frustumCulled = false; // Avoid getting clipped
+	this.scene.add( this.quad );
+
+};
+
+THREE.BloomPass.prototype = Object.assign( Object.create( THREE.Pass.prototype ), {
+
+	constructor: THREE.BloomPass,
+
+	render: function ( renderer, writeBuffer, readBuffer, delta, maskActive ) {
+
+		if ( maskActive ) renderer.context.disable( renderer.context.STENCIL_TEST );
+
+		// Render quad with blured scene into texture (convolution pass 1)
+
+		this.quad.material = this.materialConvolution;
+
+		this.convolutionUniforms[ "tDiffuse" ].value = readBuffer.texture;
+		this.convolutionUniforms[ "uImageIncrement" ].value = THREE.BloomPass.blurX;
+
+		renderer.render( this.scene, this.camera, this.renderTargetX, true );
+
+
+		// Render quad with blured scene into texture (convolution pass 2)
+
+		this.convolutionUniforms[ "tDiffuse" ].value = this.renderTargetX.texture;
+		this.convolutionUniforms[ "uImageIncrement" ].value = THREE.BloomPass.blurY;
+
+		renderer.render( this.scene, this.camera, this.renderTargetY, true );
+
+		// Render original scene with superimposed blur to texture
+
+		this.quad.material = this.materialCopy;
+
+		this.copyUniforms[ "tDiffuse" ].value = this.renderTargetY.texture;
+
+		if ( maskActive ) renderer.context.enable( renderer.context.STENCIL_TEST );
+
+		renderer.render( this.scene, this.camera, readBuffer, this.clear );
+
+	}
+
+} );
+
+THREE.BloomPass.blurX = new THREE.Vector2( 0.001953125, 0.0 );
+THREE.BloomPass.blurY = new THREE.Vector2( 0.0, 0.001953125 );
+
+
+/**
+ * @author alteredq / http://alteredqualia.com/
+ *
+ * Convolution shader
+ * ported from o3d sample to WebGL / GLSL
+ * http://o3d.googlecode.com/svn/trunk/samples/convolution.html
+ */
+
+THREE.ConvolutionShader = {
+
+	defines: {
+
+		"KERNEL_SIZE_FLOAT": "25.0",
+		"KERNEL_SIZE_INT": "25"
+
+	},
+
+	uniforms: {
+
+		"tDiffuse":        { value: null },
+		"uImageIncrement": { value: new THREE.Vector2( 0.001953125, 0.0 ) },
+		"cKernel":         { value: [] }
+
+	},
+
+	vertexShader: [
+
+		"uniform vec2 uImageIncrement;",
+
+		"varying vec2 vUv;",
+
+		"void main() {",
+
+			"vUv = uv - ( ( KERNEL_SIZE_FLOAT - 1.0 ) / 2.0 ) * uImageIncrement;",
+			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+
+		"}"
+
+	].join( "\n" ),
+
+	fragmentShader: [
+
+		"uniform float cKernel[ KERNEL_SIZE_INT ];",
+
+		"uniform sampler2D tDiffuse;",
+		"uniform vec2 uImageIncrement;",
+
+		"varying vec2 vUv;",
+
+		"void main() {",
+
+			"vec2 imageCoord = vUv;",
+			"vec4 sum = vec4( 0.0, 0.0, 0.0, 0.0 );",
+
+			"for( int i = 0; i < KERNEL_SIZE_INT; i ++ ) {",
+
+				"sum += texture2D( tDiffuse, imageCoord ) * cKernel[ i ];",
+				"imageCoord += uImageIncrement;",
+
+			"}",
+
+			"gl_FragColor = sum;",
+
+		"}"
+
+
+	].join( "\n" ),
+
+	buildKernel: function ( sigma ) {
+
+		// We lop off the sqrt(2 * pi) * sigma term, since we're going to normalize anyway.
+
+		function gauss( x, sigma ) {
+
+			return Math.exp( - ( x * x ) / ( 2.0 * sigma * sigma ) );
+
+		}
+
+		var i, values, sum, halfWidth, kMaxKernelSize = 25, kernelSize = 2 * Math.ceil( sigma * 3.0 ) + 1;
+
+		if ( kernelSize > kMaxKernelSize ) kernelSize = kMaxKernelSize;
+		halfWidth = ( kernelSize - 1 ) * 0.5;
+
+		values = new Array( kernelSize );
+		sum = 0.0;
+		for ( i = 0; i < kernelSize; ++ i ) {
+
+			values[ i ] = gauss( i - halfWidth, sigma );
+			sum += values[ i ];
+
+		}
+
+		// normalize the kernel
+
+		for ( i = 0; i < kernelSize; ++ i ) values[ i ] /= sum;
+
+		return values;
+
+	}
+
+};
+
+
+// This THREEx helper makes it easy to handle the fullscreen API
+// * it hides the prefix for each browser
+// * it hides the little discrepencies of the various vendor API
+// * at the time of this writing (nov 2011) it is available in 
+//   [firefox nightly](http://blog.pearce.org.nz/2011/11/firefoxs-html-full-screen-api-enabled.html),
+//   [webkit nightly](http://peter.sh/2011/01/javascript-full-screen-api-navigation-timing-and-repeating-css-gradients/) and
+//   [chrome stable](http://updates.html5rocks.com/2011/10/Let-Your-Content-Do-the-Talking-Fullscreen-API).
+
+// 
+// # Code
+
+//
+
+/** @namespace */
+var THREEx		= THREEx 		|| {};
+THREEx.FullScreen	= THREEx.FullScreen	|| {};
+
+/**
+ * test if it is possible to have fullscreen
+ * 
+ * @returns {Boolean} true if fullscreen API is available, false otherwise
+*/
+THREEx.FullScreen.available	= function()
+{
+	return this._hasWebkitFullScreen || this._hasMozFullScreen;
+}
+
+/**
+ * test if fullscreen is currently activated
+ * 
+ * @returns {Boolean} true if fullscreen is currently activated, false otherwise
+*/
+THREEx.FullScreen.activated	= function()
+{
+	if( this._hasWebkitFullScreen ){
+		return document.webkitIsFullScreen;
+	}else if( this._hasMozFullScreen ){
+		return document.mozFullScreen;
+	}else{
+		console.assert(false);
+	}
+}
+
+/**
+ * Request fullscreen on a given element
+ * @param {DomElement} element to make fullscreen. optional. default to document.body
+*/
+THREEx.FullScreen.request	= function(element)
+{
+	element	= element	|| document.body;
+	if( this._hasWebkitFullScreen ){
+		element.webkitRequestFullScreen();
+	}else if( this._hasMozFullScreen ){
+		element.mozRequestFullScreen();
+	}else{
+		console.assert(false);
+	}
+}
+
+/**
+ * Cancel fullscreen
+*/
+THREEx.FullScreen.cancel	= function()
+{
+	if( this._hasWebkitFullScreen ){
+		document.webkitCancelFullScreen();
+	}else if( this._hasMozFullScreen ){
+		document.mozCancelFullScreen();
+	}else{
+		console.assert(false);
+	}
+}
+
+// internal functions to know which fullscreen API implementation is available
+THREEx.FullScreen._hasWebkitFullScreen	= 'webkitCancelFullScreen' in document	? true : false;	
+THREEx.FullScreen._hasMozFullScreen	= 'mozCancelFullScreen' in document	? true : false;	
+
