f2e75ebe32ddaaf3c91d932ffa15d5830f7d13aa
lokmeinmatz <matze.kind@web.de>
2023-05-19 00:29:06 +0200

    move all to projects, create ng16 app

---
+++ page/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./views/home/home.component').then(c => c.HomeComponent)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ page/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { TuiRootModule, TuiDialogModule, TuiAlertModule } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+      BrowserAnimationsModule,
+      TuiRootModule,
+      TuiDialogModule,
+      TuiAlertModule
+],
+  providers: [ provideClientHydration() ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
